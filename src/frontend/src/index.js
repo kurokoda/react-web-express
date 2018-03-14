6466b9a879638d0cac5fb61a8d73f891791677c9
Kurokoda <indigo.developer@gmail.com>
2018-03-14 00:41:54 -0400

    Tuesday

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -7,11 +7,14 @@ import Routes from './_web/routes';
 import HeaderFooterContainer from './_web/views/layout/container/HeaderFooterContainer';
 // import DevTools from './shared/components/devTools';
 import Loading from './shared/components/Loading';
+import config from './shared/config';
 import configureStore from './shared/store';
 
 const {persistor, store} = configureStore();
 
 const rootElement = document.getElementById('root');
+
+console.log('base: ', config.API_BASE_URL);
 
 const Root = () => (
   <Provider store={store}>
